564a81992e0d7918828ffdba52e85aaaa1b78b8f
tecknut <aegan@zendesk.com>
2015-09-20 23:06:11 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject heroku-mail "0.1.0-SNAPSHOT"
+  :description "Trying out clojure with heroku for the first time. Attempting to send an e-mail."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [environ "1.0.0"]]
+  :main ^:skip-aot heroku-mail.core
+  :plugins [[environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "clojure-getting-started-standalone.jar"
+  :profiles {:production {:env {:production true}}})
---
+++ src/heroku_mail/core.clj
@@ -0,0 +1,29 @@
+(ns clojure-getting-started.web
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
+            [compojure.route :as route]
+            [clojure.java.io :as io]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
+
+(defn splash []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (pr-str ["Hello" :from 'Heroku])})
+
+(defn date []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (clj-time.format/unparse (clj-time.format/formatter "yyyy-MM-dd") (clj-time.local/loca))})
+
+(defroutes app
+  (GET "/" []
+       (splash))
+  (GET "/date" []
+        (date))
+  (ANY "*" []
+       (route/not-found (slurp (io/resource "404.html")))))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
